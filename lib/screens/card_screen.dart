2d0ac02357f52421453e15a071c82a3eb67402d1
Erick <erick.ruben.unam@gmail.com>
2022-07-24 22:52:12 -0500

    Images

--- lib/screens/card_screen.dart
+++ lib/screens/card_screen.dart
@@ -15,9 +15,16 @@ class CardScreen extends StatelessWidget {
         centerTitle: true,
       ),
       body: ListView(
-        padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 10),
+        padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
         children: const [
           CustomCardType1(),
+          SizedBox(height: 10),
+          CustomCardType2(imageUrl: 'https://photographylife.com/wp-content/uploads/2017/01/What-is-landscape-photography.jpg', name: 'Un hermoso Paisaje'),
+          SizedBox(height: 20),
+          CustomCardType2(imageUrl:'https://epsep.com.mx/wp-content/uploads/2020/10/travel-landscape-01.jpg'),
+           SizedBox(height: 20),
+          CustomCardType2(imageUrl:'https://mymodernmet.com/wp/wp-content/uploads/2020/02/Landscape-Photographer-of-the-Year-Sander-Grefte.jpg'),
+           SizedBox(height: 100),
         ]
       )
     );
