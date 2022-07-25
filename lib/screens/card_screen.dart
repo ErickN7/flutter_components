efeb7b384f05da13c1e282070778fe5492e87946
Erick <erick.ruben.unam@gmail.com>
2022-07-24 21:40:26 -0500

    first commit

---
+++ lib/screens/card_screen.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+import '../theme/app_theme.dart';
+import '../widgets/widgets.dart';
+
+class CardScreen extends StatelessWidget {
+
+  const CardScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Card Widget'),
+        centerTitle: true,
+      ),
+      body: ListView(
+        padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+        children: const [
+          CustomCardType1(),
+        ]
+      )
+    );
+  }
+}
+
